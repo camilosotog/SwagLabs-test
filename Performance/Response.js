bbbbffee37d244aabee090220230673e90bd5c0a
Camilo Soto <occamilo97@gmail.com>
2026-01-01 15:30:50 -0500

    responses clase 21

--- Performance/Response.js
+++ Performance/Response.js
@@ -11,12 +11,19 @@ export const options = {
 export default function () {
   const response = http.get('https://test.k6.io/');
   check(response, {'La página se cargó correctamente: status 200': (r) => r.status === 200})
-  // console.log('Tipos de respuesta');
+  console.log('Tipos de respuesta');
   // console.log('Información del body ' + String(response.body));
   console.log('Response url: ' + String(response.request.url));
   console.log('Response metodo: ' + String(response.request.method));
   console.log('Response status: ' + String(response.status));
   console.log('Response status_text: ' + String(response.status_text));
+  
+  // Response http (Clase 21)
+  console.log('Response Http Blocked ' + String(response.timings.blocked));
+  console.log('Response Http Conectividad ' + String(response.timings.connecting));
+  console.log('Response Http sending ' + String(response.timings.sending));
+  console.log('Response Http wait ' + String(response.timings.waiting));
+  console.log('Response Http receiving ' + String(response.timings.receiving));
 
   sleep(1);
 }
