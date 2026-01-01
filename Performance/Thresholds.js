606dc37d62a98a7d02a097006e60df356a5d7240
Camilo Soto <occamilo97@gmail.com>
2026-01-01 16:51:10 -0500

    Thresholds clase 28

--- Performance/Thresholds.js
+++ Performance/Thresholds.js
@@ -7,13 +7,16 @@ export const options = {
     { duration: '1s', target: 2 },
   ],
   thresholds: {
-    http_req_duration: ['p(95) < 110'], // El 95% de las solicitudes deben completarse en menos de 200 ms
+    http_req_duration: ['p(95) < 110'], // El 95% de las solicitudes deben completarse en menos de 110 ms
+    http_req_duration: ['min < 50'],    // El tiempo mínimo de solicitud debe ser menor a 50 ms
+    http_req_duration: ['max < 200'],   // El tiempo máximo de solicitud debe ser menor a 200 ms
+    http_req_duration: ['med < 100'],   // El tiempo mediano de solicitud debe ser menor a 100 ms
   }
 }
 
 export default function () {
   const response = http.get('https://test.k6.io/');
-  check(response, {'La página se cargó correctamente: status 200': (r) => r.status === 200})
+  // check(response, {'La página se cargó correctamente: status 200': (r) => r.status === 200})
 
   
   sleep(1);
