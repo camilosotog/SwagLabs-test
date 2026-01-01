8dee8392ceeb54ba9a15b30c54a0aa62bae346dd
Camilo Soto <occamilo97@gmail.com>
2026-01-01 16:55:57 -0500

    Thresholds clase 29

--- Performance/Thresholds.js
+++ Performance/Thresholds.js
@@ -7,10 +7,11 @@ export const options = {
     { duration: '1s', target: 2 },
   ],
   thresholds: {
-    http_req_duration: ['p(95) < 110'], // El 95% de las solicitudes deben completarse en menos de 110 ms
+    /*http_req_duration: ['p(95) < 110'], // El 95% de las solicitudes deben completarse en menos de 110 ms
     http_req_duration: ['min < 50'],    // El tiempo mínimo de solicitud debe ser menor a 50 ms
     http_req_duration: ['max < 200'],   // El tiempo máximo de solicitud debe ser menor a 200 ms
-    http_req_duration: ['med < 100'],   // El tiempo mediano de solicitud debe ser menor a 100 ms
+    http_req_duration: ['med < 100'],   // El tiempo mediano de solicitud debe ser menor a 100 ms*/
+    http_req_duration: ['p(90) < 100', 'p(95) < 130', 'p(99) < 150', 'min < 50', 'med < 100', 'max < 200'], // Múltiples percentiles para el tiempo de solicitud
   }
 }
 
