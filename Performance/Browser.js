459a6ded167368c44c0550a9435eb8bfe079686b
Camilo Soto <occamilo97@gmail.com>
2026-01-01 17:50:18 -0500

    Browser clase 61

--- Performance/Browser.js
+++ Performance/Browser.js
@@ -1,11 +1,13 @@
+import { check } from 'k6';
 import { browser } from 'k6/browser';
 
 export const options = {
   scenarios: {
     ui: {
       executor: 'shared-iterations',
-      vus: 1,
-      iterations: 1,
+      vus: 5,
+      iterations: 10,
+      maxDuration: '30s',
       options: {
         browser: {
           type: 'chromium',
@@ -23,7 +25,12 @@ export default async function () {
   const page = await browser.newPage();
   await page.goto('https://test.k6.io/');
 
-  console.log(await page.title());
+  const title = await page.title();
+  
+  const expectTitle = 'QuickPizza';
+  check(title, {
+    'El titulo es correcto': (t) => t === expectTitle,
+  })
   await page.close();
   
 }
